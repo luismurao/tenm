77ae1c403e6b2819fd67f7b54c2b1b7ff34378af
Luis Osorio <luismurao@gmail.com>
2023-01-25 18:21:24 -0600

    Added a function to read raster layer metadata

---
+++ R/metaras.R
@@ -0,0 +1,17 @@
+#' Helper function to obtain layer name from a
+#' @description Returns a character vector with the name of the raster layer
+#' @param r An object of class RasterLayer
+#' @examples
+#' tempora_layers_dir <- system.file("extdata/bio",package = "tenm")
+#' p1 <- list.files(tempora_layers_dir,full.names=TRUE,
+#'                  pattern=".tif$",recursive=TRUE)[1]
+#' r1 <- raster::raster(p1)
+#' print(metaras(r1))
+#' @export
+metaras <- function(r){
+  np <- normalizePath(r@file@name)
+  vs <- unlist(strsplit(np,split = "/"))
+  vs <- unlist(strsplit(vs[length(vs)],split="[.]"))
+  lname <- vs[1]
+  return(lname)
+}
