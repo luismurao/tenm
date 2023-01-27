f37bc55f6258508934ae65c8cca5d8e81a7d8348
Luis Osorio <luismurao@gmail.com>
2023-01-27 11:14:23 -0600

    Added regex for splitting paths in windows

--- R/metaras.R
+++ R/metaras.R
@@ -6,11 +6,14 @@
 #' p1 <- list.files(tempora_layers_dir,full.names=TRUE,
 #'                  pattern=".tif$",recursive=TRUE)[1]
 #' r1 <- raster::raster(p1)
-#' print(metaras(r1))
+#' print(tenm::metaras(r1))
 #' @export
 metaras <- function(r){
   np <- normalizePath(r@file@name)
-  vs <- unlist(strsplit(np,split = "/"))
+  sysinf <- Sys.info()
+  os <- sysinf['sysname']
+  if(os == "Windows") patt <- "\\\\" else patt <- "/"
+  vs <- unlist(strsplit(np,split = patt))
   vs <- unlist(strsplit(vs[length(vs)],split="[.]"))
   lname <- vs[1]
   return(lname)
