5df4491dfe0344727cf7115609567d3ce066fbdc
Luis Osorio <luismurao@gmail.com>
2023-09-27 14:58:50 -0600

    Allowed to passs parameters to ellpisoid_projection

--- R/00_methods.R
+++ R/00_methods.R
@@ -10,6 +10,8 @@
 #' @param layers_ext Layers extension
 #' @param mve If the projection will use the minimum volume ellipsoid algorithm
 #' @param level Proportion of data used to fit the minimum volume ellipsoid
+#' @param ... Additional parameters passed to
+#' \code{\link[tenm]{ellipsoid_projection}}
 #' @details Note that the RasterStacks in layers parameter should have the same
 #' number of elements (layers) than model_variables. The predict method will
 #' assume that variables in each RasterStack are the same as the ones in
@@ -21,7 +23,7 @@
 methods::setMethod('predict', signature(object="sp.temporal.selection"),
                    function(object, model_variables=NULL,layers = NULL,
                             layers_path=NULL,layers_ext=NULL,
-                            mve = TRUE, level=0.975){
+                            mve = TRUE, level=0.975,...){
 
 
                      mod_table <- object$mods_table
@@ -57,7 +59,8 @@ methods::setMethod('predict', signature(object="sp.temporal.selection"),
                                                              centroid = mod$centroid,
                                                              covar = mod$covariance,
                                                              level = 0.9999,
-                                                             plot = TRUE,size = 2)
+                                                             plot = TRUE,size = 2,
+                                                             ...)
                        names(projmods) <- paste0("suitability_proj")
 
 
@@ -81,7 +84,8 @@ methods::setMethod('predict', signature(object="sp.temporal.selection"),
                                                                centroid = mod$centroid,
                                                                covar = mod$covariance,
                                                                level = 0.9999,
-                                                               plot = TRUE,size = 2)
+                                                               plot = TRUE,size = 2,
+                                                               ...)
                          utils::setTxtProgressBar(pb, x, title = NULL, label = NULL)
 
                          return(suitmod)
@@ -103,7 +107,8 @@ methods::setMethod('predict', signature(object="sp.temporal.selection"),
                                                                centroid = mod$centroid,
                                                                covar = mod$covariance,
                                                                level = 0.9999,
-                                                               plot = TRUE,size = 2)
+                                                               plot = TRUE,size = 2,
+                                                               ...)
 
                          utils::setTxtProgressBar(pb, x, title = NULL, label = NULL)
 
